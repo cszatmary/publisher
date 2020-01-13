70dd1d17dd1e3b90b009959ec1b5d1402c1336f2
Christopher Szatmary <cs@christopherszatmary.com>
2020-01-12 22:54:35 -0500

    Move file related utils into file.go, implement copying files and dirs

--- util/util.go
+++ util/util.go
@@ -2,33 +2,11 @@ package util
 
 import (
 	"bytes"
-	"os"
 	"os/exec"
 	"regexp"
 
 	"github.com/pkg/errors"
-	"gopkg.in/yaml.v2"
 )
-
-func FileOrDirExists(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
-	}
-
-	return true
-}
-
-func ReadYaml(path string, val interface{}) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return errors.Wrapf(err, "failed to open file %s", path)
-	}
-	defer file.Close()
-
-	dec := yaml.NewDecoder(file)
-	err = dec.Decode(val)
-	return errors.Wrapf(err, "failed to decode yaml file %s", path)
-}
 
 func Exec(name, dir string, arg ...string) error {
 	cmd := exec.Command(name, arg...)
