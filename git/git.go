272f1763c3d78a83846d90c2b0b3a839c243faa2
Christopher Szatmary <cs@christopherszatmary.com>
2020-01-12 22:55:10 -0500

    Added functions for git add, commit and push

--- git/git.go
+++ git/git.go
@@ -3,11 +3,13 @@ package git
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/cszatma/publisher/util"
 	"github.com/pkg/errors"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 type Repository = git.Repository
@@ -23,7 +25,7 @@ func SHA(ref string) (string, error) {
 }
 
 func Clone(name, branch, path string) (*git.Repository, error) {
-	util.VerbosePrintf("Cloning repo %s to %s", name, path)
+	util.VerbosePrintf("Cloning repo %s to %s\n", name, path)
 	repo, err := git.PlainClone(path, false, &git.CloneOptions{
 		URL:           fmt.Sprintf("git@github.com:%s.git", name),
 		ReferenceName: plumbing.NewBranchReferenceName(branch),
@@ -37,7 +39,7 @@ func Clone(name, branch, path string) (*git.Repository, error) {
 }
 
 func Open(name, branch, path string) (*git.Repository, error) {
-	util.VerbosePrintf("Opening repo %s at path %s", name, path)
+	util.VerbosePrintf("Opening repo %s at path %s\n", name, path)
 	repo, err := git.PlainOpen(path)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to open repo at path %s", path)
@@ -48,7 +50,7 @@ func Open(name, branch, path string) (*git.Repository, error) {
 		return nil, errors.Wrapf(err, "failed to get worktree for repo %s", name)
 	}
 
-	util.VerbosePrintf("Cleaning %s", name)
+	util.VerbosePrintf("Cleaning %s\n", name)
 	err = wt.Clean(&git.CleanOptions{
 		Dir: true,
 	})
@@ -75,4 +77,54 @@ func Open(name, branch, path string) (*git.Repository, error) {
 	}
 
 	return repo, nil
+}
+
+func Add(name, path, addPath string) error {
+	err := util.Exec("git", path, "add", addPath)
+	return errors.Wrapf(err, "failed to add %s in repo %s", addPath, path)
+}
+
+func User() (name string, email string, err error) {
+	args := []string{"config", "--get", "--global"}
+	nameOutput, err := util.ExecOutput("git", append(args, "user.name")...)
+	if err != nil {
+		return "", "", errors.Wrap(err, "failed to get git user name")
+	}
+
+	emailOutput, err := util.ExecOutput("git", append(args, "user.email")...)
+	if err != nil {
+		return "", "", errors.Wrap(err, "failed to get git user email")
+	}
+
+	return strings.TrimSpace(nameOutput), strings.TrimSpace(emailOutput), nil
+}
+
+func Commit(name, msg string, repo *Repository) error {
+	name, email, err := User()
+	if err != nil {
+		return errors.Wrap(err, "failed to get git user info")
+	}
+
+	wt, err := repo.Worktree()
+	if err != nil {
+		return errors.Wrapf(err, "failed to get worktree for repo %s", name)
+	}
+
+	_, err = wt.Commit(msg, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  name,
+			Email: email,
+			When:  time.Now(),
+		},
+	})
+
+	return errors.Wrapf(err, "failed to commit files in repo %s", name)
+}
+
+func Push(name string, repo *Repository) error {
+	err := repo.Push(&git.PushOptions{
+		RemoteName: "origin",
+	})
+
+	return errors.Wrapf(err, "failed to push to remote in repo %s", name)
 }
