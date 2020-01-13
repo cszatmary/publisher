7ba1a3655866bdd2cf339344729958d4d06db0ae
Christopher Szatmary <cs@christopherszatmary.com>
2020-01-12 22:57:53 -0500

    Finished implementing brutalist mvp

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/cszatma/publisher/config"
@@ -37,7 +40,7 @@ func main() {
 	util.SetVerboseMode(verbose)
 	fatal.ShowStackTraces(verbose)
 
-	sourceRootPath, err := git.RootDir()
+	srcRootPath, err := git.RootDir()
 	if err != nil {
 		fatal.ExitErr(err, "Project is not a git repo")
 	}
@@ -64,25 +67,97 @@ func main() {
 		fatal.Exitf("%s is not a valid deployment target", targetName)
 	}
 
-	targetRepoPath := fmt.Sprintf("%s/%s", config.ReposDir(), target.GithubRepo)
+	// Setup target repo
+	targetRepoPath := filepath.Join(config.ReposDir(), target.GithubRepo)
 	var repo *git.Repository
 	if !util.FileOrDirExists(targetRepoPath) {
-		util.VerbosePrintf("Target repo %s does not exist, cloning...")
+		util.VerbosePrintf("Target repo %s does not exist, cloning...\n")
 
 		repo, err = git.Clone(target.GithubRepo, target.Branch, targetRepoPath)
 		if err != nil {
 			fatal.ExitErrf(err, "Failed to clone repo %s to %s", target.GithubRepo, targetRepoPath)
 		}
 
-		util.VerbosePrintf("Successfully cloned repo %s", target.GithubRepo)
+		util.VerbosePrintf("Successfully cloned repo %s\n", target.GithubRepo)
 	} else {
-		util.VerbosePrintf("Target repo %s exists, opening and setting up")
+		util.VerbosePrintf("Target repo %s exists, opening and setting up \n", target.GithubRepo)
 
 		repo, err = git.Open(target.GithubRepo, target.Branch, targetRepoPath)
 		if err != nil {
 			fatal.ExitErrf(err, "Failed to open repo %s at path %s", target.GithubRepo, targetRepoPath)
 		}
 
-		util.VerbosePrintf("Successfully opened repo %s", target.GithubRepo)
+		util.VerbosePrintf("Successfully opened repo %s\n", target.GithubRepo)
 	}
+
+	// Empty target repo
+	util.VerbosePrintf("Emptying directory %s\n", targetRepoPath)
+	dir, err := ioutil.ReadDir(targetRepoPath)
+	if err != nil {
+		fatal.ExitErr(err, "failed to read items in target dir")
+	}
+
+	for _, f := range dir {
+		// Don't remove .git dir
+		if f.Name() == ".git" && f.IsDir() {
+			continue
+		}
+
+		err = os.RemoveAll(filepath.Join(targetRepoPath, f.Name()))
+		if err != nil {
+			fatal.ExitErrf(err, "failed to remove %s", f.Name())
+		}
+	}
+
+	// Copy files
+	fmt.Println("Copying files...")
+	var files []string
+	for _, file := range conf.Files {
+		matches, err := filepath.Glob(file)
+		if err != nil {
+			fatal.ExitErr(err, "failed to parse files listed in config")
+		}
+
+		files = append(files, matches...)
+	}
+
+	for _, file := range files {
+		util.VerbosePrintf("Copying %s...\n", file)
+
+		srcPath := filepath.Join(srcRootPath, file)
+		destPath := filepath.Join(targetRepoPath, file)
+		err = util.Copy(srcPath, destPath)
+		if err != nil {
+			fatal.ExitErrf(err, "failed to copy %s to %s", srcPath, destPath)
+		}
+	}
+
+	if target.CustomURL != "" {
+		cnamePath := filepath.Join(targetRepoPath, "CNAME")
+		err = ioutil.WriteFile(cnamePath, []byte(target.CustomURL), 0644)
+		if err != nil {
+			fatal.ExitErrf(err, "Failed to write CNAME file to target repo %s", targetRepoPath)
+		}
+	}
+
+	// Commit files
+	util.VerbosePrintln("Staging files...")
+	err = git.Add(target.GithubRepo, targetRepoPath, ".")
+	if err != nil {
+		fatal.ExitErrf(err, "Failed to stage files in target repo %s", targetRepoPath)
+	}
+
+	util.VerbosePrintln("Committing files...")
+	err = git.Commit(target.GithubRepo, conf.CommitMessage, repo)
+	if err != nil {
+		fatal.ExitErrf(err, "Failed to commit files in target repo %s", targetRepoPath)
+	}
+
+	fmt.Printf("Pushing to branch %s in repo %s\n", target.Branch, target.GithubRepo)
+	err = git.Push(target.GithubRepo, repo)
+	if err != nil {
+		fatal.ExitErrf(err, "Failed to push changes to GitHub for target repo %s", targetRepoPath)
+	}
+
+	fmt.Println("Successfully published to GitHub Pages! Enjoy!")
 }
